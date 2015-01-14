eef8a39efeef412bf56a6e9ea0d3a7d4854c51bb
zit-hb <hb@zecure.org>
2015-01-14 16:50:15 +0100

    Changed code to be more like Python connector to make future updates easier.

--- src/shadowd_connector.php
+++ src/shadowd_connector.php
@@ -3,7 +3,7 @@
 /**
  * Shadow Daemon -- Web Application Firewall
  *
- *   Copyright (C) 2014 Hendrik Buchwald <hb@zecure.org>
+ *   Copyright (C) 2014-2015 Hendrik Buchwald <hb@zecure.org>
  *
  * This file is part of Shadow Daemon. Shadow Daemon is free software: you can
  * redistribute it and/or modify it under the terms of the GNU General Public
@@ -49,7 +49,7 @@ if (!function_exists('json_encode')) {
 	}
 }
 
-class config {
+class Config {
 	/* Parse a simple ini file. */
 	public function __construct() {
 		if (getenv('SHADOWD_CONNECTOR_CONFIG')) {
@@ -72,12 +72,12 @@ class config {
 	}
 
 	/* Get the value or stop if a required value is missing. */
-	public function get($key, $required = false) {
+	public function get($key, $required = false, $default = false) {
 		if (!isset($this->data[$this->section][$key])) {
 			if ($required) {
 				throw new \Exception($key . ' in config missing');
 			} else {
-				return false;
+				return $default;
 			}
 		} else {
 			return $this->data[$this->section][$key];
@@ -85,9 +85,23 @@ class config {
 	}
 }
 
-class input {
-	/* Aggregates and prepares the input. */
-	public function __construct($ignore, $caller) {
+class Input {
+	public function __construct($config) {
+		$this->config = $config;
+	}
+
+	/* Getter for the client ip. */
+	public function get_client_ip() {
+		return $_SERVER[$this->config->get('client_ip', false, 'REMOTE_ADDR')];
+	}
+
+	/* Getter for the caller. */
+	public function get_caller() {
+		return $_SERVER[$this->config->get('caller', false, 'SCRIPT_FILENAME')];
+	}
+
+	/* Aggregate and prepare the input. */
+	public function gather_input() {
 		/* Create copies of all input sources. */
 		$input_collection = array(
 			'GET' => $_GET,
@@ -111,54 +125,9 @@ class input {
 
 		/* Convert the complete input structure to a flat unique array. */
 		$this->flatten($input_collection);
-
-		/* It is a good idea to have the ability to ignore some sensitive things like passwords. */
-		if ($ignore) {
-			$this->remove_ignored($ignore, $caller);
-		}
 	}
 
-	/* Getter for the flattened input array used by the connection class. */
-	public function get() {
-		return $this->input;
-	}
-
-	/* Read in entries that should be ignored and remove them from the input. */
-	private function remove_ignored($file, $caller) {
-		$content = file_get_contents($file);
-
-		if ($content === false) {
-			throw new \Exception('could not open ignore file');
-		}
-
-		$json = json_decode($content, true);
-
-		foreach ($json as $entry) {
-			/* If there is only a caller and the caller matches delete all input. */
-			if (!isset($entry['path']) && isset($entry['caller'])) {
-				if ($caller === $entry['caller']) {
-					$this->input = array();
-
-					/* Input is empty, no need to continue with the other entries. */
-					break;
-				}
-			} else {
-				/* Skip entry if caller is set, but does not match. */
-				if (isset($entry['caller'])) {
-					if ($caller !== $entry['caller']) {
-						continue;
-					}
-				}
-
-				/* Delete the input based on its path. */
-				if (isset($entry['path'])) {
-					unset($this->input[$entry['path']]);
-				}
-			}
-		}
-	}
-
-	/* This function converts nested arrays to a flat array. */
+	/* Convert nested arrays to a flat array. */
 	private function flatten(&$input, $key = false, $path = false) {
 		/* The next part generates an unique identifier for every input element. */
 		$new_path = false;
@@ -190,63 +159,96 @@ class input {
 		}
 	}
 
-	/* Iterate over all threats and try to remove them. */
-	public function defuse(&$threats) {
-		foreach ($threats as $threat) {
-			$this->remove($threat);
+	/* Read in entries that should be ignored and remove them from the input. */
+	public function remove_ignored($file) {
+		$content = file_get_contents($file);
+
+		if ($content === false) {
+			throw new \Exception('could not open ignore file');
+		}
+
+		$json = json_decode($content, true);
+
+		foreach ($json as $entry) {
+			/* If there is only a caller and the caller matches delete all input. */
+			if (!isset($entry['path']) && isset($entry['caller'])) {
+				if ($this->get_caller() === $entry['caller']) {
+					$this->input = array();
+
+					/* Input is empty, no need to continue with the other entries. */
+					break;
+				}
+			} else {
+				/* Skip entry if caller is set, but does not match. */
+				if (isset($entry['caller'])) {
+					if ($this->get_caller() !== $entry['caller']) {
+						continue;
+					}
+				}
+
+				/* Delete the input based on its path. */
+				if (isset($entry['path'])) {
+					unset($this->input[$entry['path']]);
+				}
+			}
 		}
 	}
 
-	private function remove($path) {
-		$path_split = $this->split_path($path);
+	/* Getter for the flattened input array. */
+	public function get_input() {
+		return $this->input;
+	}
 
-		foreach ($path_split as &$key) {
-			$key = $this->unescape_key($key);
-		}
+	/* Iterate over all threats and try to remove them. */
+	public function defuse_input($threats) {
+		foreach ($threats as $path) {
+			$path_split = $this->split_path($path);
 
-		/* A valid path needs at least two pieces. */
-		if (count($path_split) < 2) {
-			return false;
-		}
+			foreach ($path_split as &$key) {
+				$key = $this->unescape_key($key);
+			}
 
-		$value = array();
-
-		/* The first element is the root path. It's not a real variable name, so we have to set it manually. */
-		$root_path = array_shift($path_split);
-
-		switch ($root_path) {
-			case 'GET':
-				$value = &$_GET;
-				break;
-			case 'POST':
-				$value = &$_POST;
-				break;
-			case 'COOKIE':
-				$value = &$_COOKIE;
-				break;
-			case 'SERVER':
-				$value = &$_SERVER;
-				break;
-			default:
-				return false;
-		}
-
-		/* Try to get the value of the path. */
-		foreach ($path_split as $name) {
-			/* Stop if the next layer does not exist. */
-			if (!isset($value[$name])) {
+			/* A valid path needs at least two pieces. */
+			if (count($path_split) < 2) {
 				return false;
 			}
 
-			/* Change the value reference to the next element. */
-			$value = &$value[$name];
+			$value = array();
+
+			/* The first element is the root path. It's not a real variable name, so we have to set it manually. */
+			$root_path = array_shift($path_split);
+
+			switch ($root_path) {
+				case 'GET':
+					$value = &$_GET;
+					break;
+				case 'POST':
+					$value = &$_POST;
+					break;
+				case 'COOKIE':
+					$value = &$_COOKIE;
+					break;
+				case 'SERVER':
+					$value = &$_SERVER;
+					break;
+				default:
+					continue;
+			}
+
+			/* Try to get the value of the path. */
+			foreach ($path_split as $name) {
+				/* Stop if the next layer does not exist. */
+				if (!isset($value[$name])) {
+					break;
+				}
+
+				/* Change the value reference to the next element. */
+				$value = &$value[$name];
+			}
+
+			/* Finally the threat can be removed. */
+			$value = '';
 		}
-
-		/* Finally the threat can be removed. */
-		$value = '';
-
-		/* Success. */
-		return true;
 	}
 
 	/**
@@ -259,14 +261,17 @@ class input {
 		return str_replace(array('\\', '|'), array('\\\\', '\\|'), $key);
 	}
 
+	/* Escaped keys have to be unescaped before they can be defused. */
 	private function unescape_key($key) {
 		return str_replace(array('\\\\', '\\|'), array('\\', '|'), $key);
 	}
 
+	/* Split path at dash, except if it is escaped. */
 	public function split_path($path) {
 		return preg_split('/\\\\.(*SKIP)(*FAIL)|\|/s', $path);
 	}
 
+	/* Strip slashes recursively if magic_quotes_gpc is enabled. */
 	private function stripslashes(&$input) {
 		if (is_array($input)) {
 			return array_walk($input, array($this, 'stripslashes'));
@@ -276,27 +281,17 @@ class input {
 	}
 }
 
-class connection {
-	private $output = '';
-
-	public function __construct($profile, $host, $port, $key, $ssl) {
-		$this->profile = $profile;
-		$this->host = $host;
-		$this->port = $port;
-		$this->key = $key;
-		$this->ssl = $ssl;
-	}
-
-	public function send(input &$input, $client_ip, $caller) {
+class Connection {
+	public function send(input &$input, $host, $port, $profile, $key, $ssl) {
 		$context = stream_context_create();
 
-		if ($this->ssl) {
+		if ($ssl) {
 			$result = stream_context_set_option($context, 'ssl', 'verify_host', true);
 			$result = stream_context_set_option($context, 'ssl', 'cafile', $this->ssl);
 			$result = stream_context_set_option($context, 'ssl', 'verify_peer', true);
 		}
 
-		$resource = ($this->ssl ? 'ssl' : 'tcp') . '://' . $this->host . ':' . $this->port;
+		$resource = ($ssl ? 'ssl' : 'tcp') . '://' . $host . ':' . $port;
 		$fp = @stream_socket_client($resource, $errno, $errstr, 5, STREAM_CLIENT_CONNECT, $context);
 
 		if (!$fp) {
@@ -305,33 +300,37 @@ class connection {
 			} else {
 				throw new \Exception('unknown network error');
 			}
-		} else {
-			$data = array(
-				'version' => SHADOWD_CONNECTOR_VERSION,
-				'client_ip' => $client_ip,
-				'caller' => $caller,
-				'input' => $input->get()
-			);
-
-			/**
-			 * Input format:
-			 *   profile_id\n
-			 *   hmac(json)\n
-			 *   json\n
-			 */
-			$json = json_encode($data);
-			fwrite($fp, $this->profile . "\n" . $this->sign($json) . "\n" . $json . "\n");
-
-			while (!feof($fp)) {
-				$this->output .= fgets($fp, 1024);
-			}
-
-			fclose($fp);
 		}
+
+		$data = array(
+			'version' => SHADOWD_CONNECTOR_VERSION,
+			'client_ip' => $input->get_client_ip(),
+			'caller' => $input->get_caller(),
+			'input' => $input->get_input()
+		);
+
+		/**
+		 * Input format:
+		 *   profile_id\n
+		 *   hmac(json)\n
+		 *   json\n
+		 */
+		$json = json_encode($data);
+		fwrite($fp, $profile . "\n" . $this->sign($key, $json) . "\n" . $json . "\n");
+
+		$output = '';
+
+		while (!feof($fp)) {
+			$output .= fgets($fp, 1024);
+		}
+
+		fclose($fp);
+
+		return $this->parse_output($output);
 	}
 
-	public function get_threats() {
-		$json = json_decode($this->output, true);
+	private function parse_output($output) {
+		$json = json_decode($output, true);
 
 		switch ($json['status']) {
 			case STATUS_OK:
@@ -349,59 +348,47 @@ class connection {
 		}
 	}
 
-	private function sign($json) {
-		return hash_hmac('sha256', $json, $this->key);
+	private function sign($key, $json) {
+		return hash_hmac('sha256', $json, $key);
 	}
 }
 
-/**
- * This class glues all other classes together and keeps everyone working. It also avoids pollution.
- * The complete process requires 6 steps. If something unexpected happens an error is logged and if
- * the protection is enabled the execution of the process is stopped.
- * So if you want to write an own connector for another language this is what you have to do.
- */
-class connector {
+class Connector {
 	public static function start() {
 		try {
-			/* Step 1: Get the configuration. */
-			$config = new config();
+			/* Get the configuration. */
+			$config = new Config();
 
-			$client_ip = ($config->get('client_ip') ? @$_SERVER[$config->get('client_ip')] : $_SERVER['REMOTE_ADDR']);
-			$caller = ($config->get('prepend_name') ? $_SERVER['SERVER_NAME'] . ':' : '') .
-				($config->get('caller') ? @$_SERVER[$config->get('caller')] : $_SERVER['SCRIPT_FILENAME']);
+			/* Get the input in a flat format, but without sensible data. */
+			$input = new Input($config);
 
-			/* Step 2: Get the input in a flat format, but without sensible data. */
-			$input = new input($config->get('ignore'), $caller);
+			$input->gather_input();
 
-			/**
-			 * Step 3: Establish a tcp connection with a shadowd server, send the encoded input + hmac
-			 * and save the encoded answer in a class attribute.
-			 */
-			$connection = new connection(
+			$ignored = $config->get('ignored');
+			if ($ignored) {
+				$input->remove_ignored($ignored);
+			}
+
+			/* Establish a tcp connection with a shadowd server and send the user input. */
+			$connection = new Connection();
+
+			$threats = $connection->send(
+				$input,
+				$config->get('host', false, '127.0.0.1'),
+				$config->get('port', false, '9115'),
 				$config->get('profile', true),
-				($config->get('host') ? $config->get('host') : '127.0.0.1'),
-				($config->get('port') ? $config->get('port') : '9115'),
 				$config->get('key', true),
 				$config->get('ssl')
 			);
 
-			$connection->send(
-				$input,
-				$client_ip,
-				$caller
-			);
-
-			/* Step 4: Decode the answer and extract threats. */
-			$threats = $connection->get_threats();
-
-			/* Step 5: If observe mode is disabled eliminate the threats. */
+			/* If observe mode is disabled eliminate the threats. */
 			if (!$config->get('observe') && $threats) {
-				$input->defuse($threats);
+				$input->defuse_input($threats);
 			}
 
-			/* Step 6: If debug is enabled drop a log message (for fail2ban f.i.). */
+			/* If debug is enabled drop a log message (for fail2ban f.i.). */
 			if ($config->get('debug') && $threats) {
-				error_log('shadowd: removed threat from client: ' . $client_ip);
+				error_log('shadowd: removed threat from client: ' . $input->get_client_ip());
 			}
 		} catch (\Exception $e) {
 			/* Let PHP handle the log writing if debug is enabled. */
