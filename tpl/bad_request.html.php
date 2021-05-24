e46462f26d2089bc12096a986bb4936ce5b566a8
zit-hb <hb@zecure.org>
2021-05-24 15:19:12 +0000

    Remove wrong message from bad request template

--- tpl/bad_request.html.php
+++ tpl/bad_request.html.php
@@ -1,6 +1,2 @@
 <p>Your request was rejected by the <a href="https://shadowd.zecure.org">Shadow Daemon</a> web application firewall.</p>
 <p>To prevent harm it was blocked. If you think this is an error please contact your administrator.</p>
-
-<?php if (!$this->isDebug()): ?>
-    <p>Enable the debug setting to get additional information about the corrupted file.</p>
-<?php endif; ?>
