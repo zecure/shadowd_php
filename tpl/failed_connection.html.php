9a441e0d1fd215c76d653f8a77b5034916b13bde
zit-hb <hb@zecure.org>
2021-05-24 10:44:11 +0000

    Improve error messages

--- tpl/failed_connection.html.php
+++ tpl/failed_connection.html.php
@@ -1,12 +1,9 @@
-<p>
-    No connection could be established with the <a href="https://shadowd.zecure.org">Shadow Daemon</a> server.
-    <?php if (!empty($this->getExceptionMessage())): ?>
-        The server is either unreachable or the wrong address is specified in the configuration file of the <a href="https://shadowd.zecure.org/documentation/connectors/">connector</a>.
-    <?php else: ?>
-        The exact cause is not known. It might be related to an invalid SSL public key.
+<p>No connection could be established with the <a href="https://shadowd.zecure.org">Shadow Daemon</a> server.</p>
+<?php if (!empty($this->getExceptionMessage())): ?>
+    <p>The server is either unreachable or the wrong address is specified in the configuration file of the <a href="https://shadowd.zecure.org/documentation/connectors/">connector</a>.</p>
+    <?php if (!$this->isDebug()): ?>
+        <p>Enable the debug setting to get additional information.</p>
     <?php endif; ?>
-</p>
-
-<?php if (!$this->isDebug()): ?>
-    <p>Enable the debug setting to get additional information.</p>
+<?php else: ?>
+    <p>The exact cause is not known. It might be related to an invalid SSL public key.</p>
 <?php endif; ?>
